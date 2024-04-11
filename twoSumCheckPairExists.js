92d560576d25f0552987cc5d3c3a9284ecd68b31
Vaibhav Rai <raivaibhav08@gmail.com>
2024-04-11 11:22:52 +0530

    Fix: twoSum algorithm

--- twoSumCheckPairExists.js
+++ twoSumCheckPairExists.js
@@ -5,26 +5,38 @@
 
 
 
-function TwoSum(arr, k) {
-  const newArr = arr.sort((a,b) => a-b);
-  let i = 0;
-  let j = arr.length - 1;
-  let sum = 0;
-  while(i<j) {
-      sum = arr[i] + arr[j];
-      
-      if ((sum === k)) {
-          break;
-      }
-      if (sum > k){
-          j--;
-      } else {
-          i++
-      }
-  };
-  if (j === i) {
-      return [-1, -1]
-  } 
-  return [i, j]
-}
-console.log(TwoSum([2, 5, 6, 8, 11], 0))
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let i = 0;
+    let j = nums.length - 1;
+    let sum = 0;
+    nums = nums.map((i, index) => ({
+        val: i,
+        index,
+    }))
+    let first = 0;
+    let second = 0;
+    nums.sort((a, b) => a.val - b.val)
+    while(i<j) {
+        sum = nums[i].val + nums[j].val;
+        
+        if ((sum === target)) {
+            first = nums[i].index;
+            second = nums[j].index;
+            break;
+        }
+        if (sum > target){
+            j--;
+        } else {
+            i++
+        }
+    };
+    if (j === i) {
+        return [-1, -1]
+    } 
+    return [first, second]
+};
